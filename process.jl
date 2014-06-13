9373f88f8fa07903054d876fcac128a1e36eee5b
jverzani <jverzani@gmail.com>
2014-06-13 13:14:16 -0400

    updates

---
+++ process.jl
@@ -0,0 +1,14 @@
+## process html files
+
+## glob .jmd files
+using Glob
+jmd_files = glob("*.jmd")
+html_files = map(x -> replace(x, r"jmd$", "html"), jmd_files)
+
+for (j,h) in zip(jmd_files, html_files)
+    if mtime(h) == 0.0 || (mtime(j) > mtime(h))	
+      println("process $j")
+      cmd = "using Weave; markdownToHtml(\"$j\", out=\"$h\");"
+      run(`julia -e "$cmd"`)  
+    end    		       
+end
